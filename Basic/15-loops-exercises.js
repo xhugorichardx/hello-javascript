eb48f4269793c02981076f73170920c1c1744d77
xhrx <xhugorichardx@gmail.com>
2025-07-17 02:45:30 -0500

    Resolvieron Ejercicios leccion 15

--- Basic/15-loops-exercises.js
+++ Basic/15-loops-exercises.js
@@ -1,23 +1,41 @@
-/*
-Clase 30 - Ejercicios: Bucles
-Vídeo: https://youtu.be/1glVfFxj8a4?t=12732
-*/
-
-// NOTA: Explora diferentes sintaxis de bucles para resolver los ejercicios
-
-// 1. Crea un bucle que imprima los números del 1 al 20
-
+for (let i = 1; i <= 20; i++) {
+    console.log('va la cuenta en: ' + i);
+}
 // 2. Crea un bucle que sume todos los números del 1 al 100 y muestre el resultado
 
+let suma = 0;
+for (let i = 1; i <= 100; i++) {
+suma = suma + i;
+console.log(suma)
+}
 // 3. Crea un bucle que imprima todos los números pares entre 1 y 50
+for (let i = 1; i <= 50; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
 
 // 4. Dado un array de nombres, usa un bucle para imprimir cada nombre en la consola
+let nombres = ['Juan', 'Maria', 'Pedro', 'Ana', 'Luis'];
 
-// 5. Escribe un bucle que cuente el número de vocales en una cadena de texto
+for (let nombre of nombres) {
+    console.log(nombre);
+}
 
 // 6. Dado un array de números, usa un bucle para multiplicar todos los números y mostrar el producto
 
+let numeros = [1, 2, 3, 4, 5];
+let producto = 1;
+for (let i = 0; i < numeros.length; i++) {
+    producto = producto * numeros[i];   
+}
+console.log(producto);
+
 // 7. Escribe un bucle que imprima la tabla de multiplicar del 5
+
+for (let i = 1; i <= 10; i++) {
+    console.log (5*i);
+}
 
 // 8. Usa un bucle para invertir una cadena de texto
 
